499190db86bdf6dd018f346c3d3703092a82c748
João Coimbra <joao.benatti.coimbra@gmail.com>
2023-03-21 09:43:59 -0300

    Update index.py

--- index.py
+++ index.py
@@ -10,8 +10,9 @@ def display(request):
     return response.file('views/index.html')
 
 
-# string -> str, Sanic in Vercel dont support str, str in Sanic ^21
-@app.route("/download/<filename:str>")
+# string -> str, Vercel doesn't support Sanic 21.12, only up to 19.6
+# C:\Python310\lib\site-packages\sanic_routing\route.py:355: DeprecationWarning: Use of 'string' as a path parameter type is deprected, and will be removed in Sanic v21.12. Instead, use <filename:str>.
+@app.route("/download/<filename:string>")
 def ador(request, filename: str):
     return response.file('static/'+filename)
 
