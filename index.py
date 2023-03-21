6f052d32868d63140d671ddd7856b33f21408e1d
João Coimbra <joao.benatti.coimbra@gmail.com>
2023-03-21 09:23:48 -0300

    Update index.py

--- index.py
+++ index.py
@@ -10,13 +10,13 @@ def display(request):
     return response.file('views/index.html')
 
 
-@app.route("/download")
-async def download(request):
-    return response.json({'message': 'Could not download'})
-
+# string -> str, Sanic in Vercel dont support str, str in Sanic ^21
 @app.route("/download/<filename:string>")
 def ador(request, filename: str):
-    return response.file('static/'+filename)
+    try:
+        return response.file('static/'+filename)
+    except Exception:
+        return response.text('The file could not be found', status=404)
 
 
 if __name__ == "__main__":
