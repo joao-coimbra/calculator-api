89211b0e182d8e7bf8b15f36725295e46a0f10ff
João Coimbra <joao.benatti.coimbra@gmail.com>
2023-03-23 13:04:06 -0300

    fix and make it better

--- index.py
+++ index.py
@@ -4,15 +4,11 @@ from api import api
 app = Sanic(__name__)
 app.blueprint(api)
 
-
-@app.route("/")
-def display(request):
-    return response.file('views/index.html')
-
+app.static("/", "views/index.html")
 
 # string -> str, Vercel doesn't support Sanic 21.12, only up to 19.6
 # C:\Python310\lib\site-packages\sanic_routing\route.py:355: DeprecationWarning: Use of 'string' as a path parameter type is deprected, and will be removed in Sanic v21.12. Instead, use <filename:str>.
-@app.route("/download/<filename:string>")
+@app.route("/download/<filename>")
 def ador(request, filename: str):
     return response.file('static/'+filename)
 
